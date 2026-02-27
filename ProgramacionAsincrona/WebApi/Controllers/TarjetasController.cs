96628ac0677d9e9ab4bc56760017a3e9e37b21e7
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-02-26 23:37:42 -0600

    Task.WhenAll

---
+++ ProgramacionAsincrona/WebApi/Controllers/TarjetasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Helpers;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarjetasController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> ProcesarTarjeta([FromBody] string tarjeta)
+        {
+            var valorAleatorio = RandomGen.NextDouble();
+            var aprobada = valorAleatorio > 0.1;
+            await Task.Delay(1000);
+            Console.WriteLine($"Tarjeta {tarjeta} procesada");
+            return Ok(new { Tarjeta = tarjeta, Aprobada = aprobada });
+        }
+    }
+}
--- ProgramacionAsincrona/WinFormsApp/Form1.cs
+++ ProgramacionAsincrona/WinFormsApp/Form1.cs
@@ -1,3 +1,6 @@
+using Newtonsoft.Json;
+using System.Diagnostics;
+
 namespace WinFormsApp
 {
     public partial class Form1 : Form
@@ -21,21 +24,51 @@ namespace WinFormsApp
         private async void btnIniciar_Click(object sender, EventArgs e)
         {
             loadingGif.Visible = true;
-            await Esperar();
-            var nombre = txtInput.Text;
+
+            var tarjetas = ObtenerTarjetasDeCredito(25000);
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
 
             try
             {
-                var saludo = await ObtenerSaludo(nombre);
-                MessageBox.Show(saludo);
+                await ProcesarTarjetas(tarjetas);
             }
             catch(HttpRequestException ex)
             {
                 MessageBox.Show(ex.Message);
             }
-            
-            
+
+            stopwatch.Stop();
+            MessageBox.Show($"Operación finalizada en: {stopwatch.Elapsed.TotalSeconds} segundos");
+
             loadingGif.Visible = false;
+        }
+
+        private async Task ProcesarTarjetas(List<string> tarjetas)
+        {
+            var tareas = new List<Task>();
+
+            foreach (var tarjeta in tarjetas)
+            {
+                var json = JsonConvert.SerializeObject(tarjeta);
+                var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+                var respuestaTask = _httpClient.PostAsync($"{_apiUrl}/api/Tarjetas", content);
+                tareas.Add(respuestaTask);
+            }
+
+            await Task.WhenAll(tareas);
+        }
+
+        private List<string> ObtenerTarjetasDeCredito(int cantidadDeTarjetas)
+        {
+            var tarjetas = new List<string>();
+
+            for (int i = 0; i < cantidadDeTarjetas; i++)
+            {
+                tarjetas.Add(i.ToString().PadLeft(16, '0'));
+            }
+
+            return tarjetas;
         }
 
         private async Task Esperar()
