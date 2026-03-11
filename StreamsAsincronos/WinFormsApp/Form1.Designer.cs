4d82bfeff4a0f9cead210cdee4d90111dddc2b58
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-03-10 23:57:03 -0600

    Se crea proyecto para streams asíncronos

---
+++ StreamsAsincronos/WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btnIniciar = new Button();
+            loadingGif = new PictureBox();
+            txtInput = new TextBox();
+            label1 = new Label();
+            pgProcesamiento = new ProgressBar();
+            btnCancelar = new Button();
+            ((System.ComponentModel.ISupportInitialize)loadingGif).BeginInit();
+            SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            btnIniciar.Location = new Point(41, 107);
+            btnIniciar.Name = "btnIniciar";
+            btnIniciar.Size = new Size(229, 29);
+            btnIniciar.TabIndex = 0;
+            btnIniciar.Text = "Iniciar Proceso";
+            btnIniciar.UseVisualStyleBackColor = true;
+            btnIniciar.Click += btnIniciar_Click;
+            // 
+            // loadingGif
+            // 
+            loadingGif.Image = (Image)resources.GetObject("loadingGif.Image");
+            loadingGif.Location = new Point(41, 160);
+            loadingGif.Name = "loadingGif";
+            loadingGif.Size = new Size(229, 226);
+            loadingGif.SizeMode = PictureBoxSizeMode.Zoom;
+            loadingGif.TabIndex = 1;
+            loadingGif.TabStop = false;
+            loadingGif.Visible = false;
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(111, 41);
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(271, 27);
+            txtInput.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(41, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Nombre";
+            // 
+            // pgProcesamiento
+            // 
+            pgProcesamiento.Location = new Point(296, 173);
+            pgProcesamiento.Name = "pgProcesamiento";
+            pgProcesamiento.Size = new Size(403, 29);
+            pgProcesamiento.TabIndex = 4;
+            pgProcesamiento.Visible = false;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(296, 107);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(94, 29);
+            btnCancelar.TabIndex = 5;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.Click += btnCancelar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCancelar);
+            Controls.Add(pgProcesamiento);
+            Controls.Add(label1);
+            Controls.Add(txtInput);
+            Controls.Add(loadingGif);
+            Controls.Add(btnIniciar);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)loadingGif).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnIniciar;
+        private PictureBox loadingGif;
+        private TextBox txtInput;
+        private Label label1;
+        private ProgressBar pgProcesamiento;
+        private Button btnCancelar;
+    }
+}
---
+++ StreamsAsincronos/WinFormsApp/Form1.cs
@@ -0,0 +1,33 @@
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _apiUrl;
+        private readonly HttpClient _httpClient;
+        private CancellationTokenSource _cancellationTokenSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _apiUrl = "https://localhost:7186";
+            _httpClient = new HttpClient();
+        }
+        
+        private async void btnIniciar_Click(object sender, EventArgs e)
+        {
+            loadingGif.Visible = true;
+
+
+
+            loadingGif.Visible = false;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            _cancellationTokenSource?.Cancel();
+        }
+    }
+}
