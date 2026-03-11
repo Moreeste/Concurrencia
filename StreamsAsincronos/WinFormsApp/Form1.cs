38fe413bc031b84194350dc6d35091423c40a97b
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-03-11 00:09:29 -0600

    IAsyncEnumerable

--- StreamsAsincronos/WinFormsApp/Form1.cs
+++ StreamsAsincronos/WinFormsApp/Form1.cs
@@ -1,6 +1,3 @@
-using Newtonsoft.Json;
-using System.Diagnostics;
-
 namespace WinFormsApp
 {
     public partial class Form1 : Form
@@ -20,9 +17,30 @@ namespace WinFormsApp
         {
             loadingGif.Visible = true;
 
+            //foreach (var nombre in GenerarNombres())
+            //{
+            //    Console.WriteLine(nombre);
+            //}
 
+            await foreach (var nombre in GenerarNombresAsync())
+            {
+                Console.WriteLine(nombre);
+            }
 
             loadingGif.Visible = false;
+        }
+
+        private IEnumerable<string> GenerarNombres()
+        {
+            yield return "Esteban";
+            yield return "Alejandra";
+        }
+
+        private async IAsyncEnumerable<string> GenerarNombresAsync()
+        {
+            yield return "Esteban";
+            await Task.Delay(2000);
+            yield return "Alejandra";
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
