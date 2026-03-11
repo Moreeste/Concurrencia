91c6d789495a8ec8a88beff185206be3d86e63b3
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-03-11 00:20:15 -0600

    Cancelar streams asíncronos

--- StreamsAsincronos/WinFormsApp/Form1.cs
+++ StreamsAsincronos/WinFormsApp/Form1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace WinFormsApp
 {
     public partial class Form1 : Form
@@ -17,15 +19,33 @@ namespace WinFormsApp
         {
             loadingGif.Visible = true;
 
+            //_cancellationTokenSource = new CancellationTokenSource();
+
             //foreach (var nombre in GenerarNombres())
             //{
             //    Console.WriteLine(nombre);
             //}
 
-            await foreach (var nombre in GenerarNombresAsync())
-            {
-                Console.WriteLine(nombre);
-            }
+            //try
+            //{
+            //    await foreach (var nombre in GenerarNombresAsync(_cancellationTokenSource.Token))
+            //    {
+            //        Console.WriteLine(nombre);
+            //    }
+            //}
+            //catch (TaskCanceledException ex)
+            //{
+            //    Console.WriteLine("Operación Cancelada");
+            //}
+            //finally
+            //{
+            //    _cancellationTokenSource?.Dispose();
+            //}
+
+            var nombresEnumerable = GenerarNombresAsync();
+            await ProcesarNombres(nombresEnumerable);
+
+            Console.WriteLine("Fin");
 
             loadingGif.Visible = false;
         }
@@ -36,11 +56,34 @@ namespace WinFormsApp
             yield return "Alejandra";
         }
 
-        private async IAsyncEnumerable<string> GenerarNombresAsync()
+        private async IAsyncEnumerable<string> GenerarNombresAsync([EnumeratorCancellation] CancellationToken token = default)
         {
             yield return "Esteban";
-            await Task.Delay(2000);
+            await Task.Delay(2000, token);
             yield return "Alejandra";
+            await Task.Delay(2000, token);
+            yield return "Carol";
+        }
+
+        private async Task ProcesarNombres(IAsyncEnumerable<string> nombresEnumerable)
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                await foreach (var nombre in nombresEnumerable.WithCancellation(_cancellationTokenSource.Token))
+                {
+                    Console.WriteLine(nombre);
+                }
+            }
+            catch (TaskCanceledException ex)
+            {
+                Console.WriteLine("Operación Cancelada");
+            }
+            finally
+            {
+                _cancellationTokenSource?.Dispose();
+            }
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
