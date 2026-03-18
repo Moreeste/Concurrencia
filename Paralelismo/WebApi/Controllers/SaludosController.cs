ace9b3735be0cfc5eed91eb1f51dd58d76332149
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-03-17 23:15:18 -0600

    Creación de proyecto de paralelismo

---
+++ Paralelismo/WebApi/Controllers/SaludosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaludosController : ControllerBase
+    {
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<string>> ObtenerSaludo(string nombre)
+        {
+            Console.WriteLine($"Hilo antes del await: {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            Console.WriteLine($"Hilo después del await: {Thread.CurrentThread.ManagedThreadId}");
+
+            return $"Hola, {nombre}!";
+        }
+    }
+}
