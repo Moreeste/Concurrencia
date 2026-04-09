da35b4fdd9c0ff749d65e1bebb7a89abe42b692d
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-04-09 01:02:46 -0600

    Deadlock

--- AntipatronesParalelismo/WinFormsApp/Form1.cs
+++ AntipatronesParalelismo/WinFormsApp/Form1.cs
@@ -21,30 +21,72 @@ namespace WinFormsApp
             loadingGif.Visible = true;
             Console.WriteLine("Inicio");
 
-            var mutex = new object();
-            Random random = new Random();
-            var diccionarioConcurrente = new ConcurrentDictionary<double, int>();
+            var mutexA = new object();
+            var mutexB = new object();
 
-            Parallel.For(1, 1000000, i =>
+            var tarea1 = Task.Run(() =>
             {
-                double llave;
-                lock (mutex)
+                Parallel.For(1, 100000, i =>
                 {
-                    llave = random.NextDouble();
-                }
-                diccionarioConcurrente.AddOrUpdate(llave, 1, (llave, valorAnterior) => valorAnterior + 1);
+                    lock (mutexA)
+                    {
+                        lock (mutexB)
+                        {
+                            var valor = i;
+                        }
+                    }
+                });
             });
 
-            var masFrecuentes = diccionarioConcurrente.OrderByDescending(x => x.Value).Take(5).ToList();
-
-            foreach (var item in masFrecuentes)
+            var tarea2 = Task.Run(() =>
             {
-                Console.WriteLine($"Valor: {item.Key}, Frecuencia: {item.Value}");
-            }
+                Parallel.For(1, 100000, i =>
+                {
+                    lock (mutexB)
+                    {
+                        lock (mutexA)
+                        {
+                            var valor = i;
+                        }
+                    }
+                });
+            });
+
+            await Task.WhenAll(tarea1, tarea2);
 
             Console.WriteLine("Fin");
             loadingGif.Visible = false;
         }
+
+        //private async void btnIniciar_Click(object sender, EventArgs e)
+        //{
+        //    loadingGif.Visible = true;
+        //    Console.WriteLine("Inicio");
+
+        //    var mutex = new object();
+        //    Random random = new Random();
+        //    var diccionarioConcurrente = new ConcurrentDictionary<double, int>();
+
+        //    Parallel.For(1, 1000000, i =>
+        //    {
+        //        double llave;
+        //        lock (mutex)
+        //        {
+        //            llave = random.NextDouble();
+        //        }
+        //        diccionarioConcurrente.AddOrUpdate(llave, 1, (llave, valorAnterior) => valorAnterior + 1);
+        //    });
+
+        //    var masFrecuentes = diccionarioConcurrente.OrderByDescending(x => x.Value).Take(5).ToList();
+
+        //    foreach (var item in masFrecuentes)
+        //    {
+        //        Console.WriteLine($"Valor: {item.Key}, Frecuencia: {item.Value}");
+        //    }
+
+        //    Console.WriteLine("Fin");
+        //    loadingGif.Visible = false;
+        //}
 
         //private async void btnIniciar_Click(object sender, EventArgs e)
         //{
